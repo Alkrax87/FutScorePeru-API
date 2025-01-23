3e67e4f2ae51f27bfe461f3e92eab5de89c986b7
Alkrax87 <miguelavp_hl@outlook.com>
2025-01-22 19:48:04 -0500

    Remove required country field from Manager model schema

--- src/models/Manager.js
+++ src/models/Manager.js
@@ -5,7 +5,6 @@ const ManagerSchema = new mongoose.Schema(
     managerId: { type: Number, required: true },
     teamId: { type: String, required: true },
     name: { type: String, required: true },
-    country: { type: String, required: true },
     cod: { type: String, required: true },
     photo: { type: String, required: true },
   },
