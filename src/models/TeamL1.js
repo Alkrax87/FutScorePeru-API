6867cb9435910542f184ccf6001d3eef1e00ddb0
Alkrax87 <miguelavp_hl@outlook.com>
2025-02-09 11:16:26 -0500

    Refactor Team models to remove unused fields and standardize stadium property

--- src/models/TeamL1.js
+++ src/models/TeamL1.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const TeamL1Schema = new mongoose.Schema(
   {
-    category: { type: String, required: true },
     teamId: { type: String, unique: true, required: true },
     name: { type: String, required: true },
     abbreviation: { type: String, required: true },
@@ -11,25 +10,11 @@ const TeamL1Schema = new mongoose.Schema(
     alt: { type: String, required: true },
     url: { type: String, required: true },
     location: { type: String, required: true },
+    stadium: { type: Number, required: true },
     color: {
       c1: { type: String, required: true },
       c2: { type: String, required: false },
-    },
-    stadium: {
-      url: { type: String, required: true },
-    },
-    manager: {
-      url: { type: String, required: true },
-    },
-    lastgames: {
-      url: { type: String, required: true },
-    },
-    performance: {
-      url: { type: String, required: true },
-    },
-    results: {
-      url: { type: String, required: true },
-    },
+    }
   },
   {
     collection: "teamsLiga1",
--- src/models/TeamL3.js
+++ src/models/TeamL3.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const TeamL3Schema = new mongoose.Schema(
   {
-    category: { type: String, required: true },
-    group: { type: String, required: true },
     teamId: { type: String, unique: true, required: true },
+    group: { type: String, required: true },
     name: { type: String, required: true },
     abbreviation: { type: String, required: true },
     image: { type: String, required: true },
@@ -12,25 +11,11 @@ const TeamL3Schema = new mongoose.Schema(
     alt: { type: String, required: true },
     url: { type: String, required: true },
     location: { type: String, required: true },
+    stadium: { type: Number, required: true },
     color: {
       c1: { type: String, required: true },
       c2: { type: String, required: false },
-    },
-    stadium: {
-      url: { type: String, required: true },
-    },
-    manager: {
-      url: { type: String, required: true },
-    },
-    lastgames: {
-      url: { type: String, required: true },
-    },
-    performance: {
-      url: { type: String, required: true },
-    },
-    results: {
-      url: { type: String, required: true },
-    },
+    }
   },
   {
     collection: "teamsLiga3",
