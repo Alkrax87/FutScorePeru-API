988a67c5580c64ab8b69f92c5ede28ff44b76992
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-06-14 19:52:45 -0500

    Add getFixtureByStageAndRound endpoint to retrieve matches by category, stage, and round

--- src/controllers/fixtureController.js
+++ src/controllers/fixtureController.js
@@ -60,6 +60,77 @@ const getFixtureByTeamId = async (req, res) => {
   }
 }
 
+const getFixtureByStageAndRound = async (req, res) => {
+  try {
+    const { category, stage, round  } = req.params;
+
+    const validStage = ["apertura", "clausura", "regional", "grupos", "final"];
+
+    if (!validStage.includes(stage)) {
+      return res.status(400).json({ error: "Invalid stage" });
+    }
+
+    const fixture = await Fixture.findOne({
+      category: category,
+    }).select(
+      "-_id -category"
+    );
+
+    if (!fixture) {
+      return res.status(404).json({ error: "Fixture not found" });
+    }
+
+    const roundIndex = parseInt(round) - 1;
+    if (isNaN(roundIndex) || roundIndex < 0) {
+      return res.status(400).json({ error: "Invalid round" });
+    }
+
+    let stageNames = [];
+
+    switch (category) {
+      case '1':
+        stageNames = [stage];
+        break;
+      case '2':
+        if (stage === "regional") {
+          stageNames = ["regionalA", "regionalB"];
+        } else if (stage === "grupos") {
+          stageNames = ["gruposPromotionA", "gruposPromotionB", "gruposRelegation"];
+        }
+        break;
+      case '3':
+        if (stage === "regional") {
+          stageNames = ["regional1", "regional2", "regional3", "regional4"];
+        } else if (stage === "final") {
+          stageNames = ["finalA", "finalB", "finalC", "finalD"];
+        }
+        break;
+      default:
+        break;
+    }
+
+    const matchingStages = fixture.stages.filter(stage => stageNames.includes(stage.name));
+
+    if (!matchingStages || matchingStages.length === 0) {
+      return res.status(400).json({ error: "Stages not found" });
+    }
+
+    const filterMatches = matchingStages
+      .map(stage => stage.matches[roundIndex])
+      .filter(matches => matches && matches.length > 0)
+      .flat();
+
+    if (filterMatches.length === 0) {
+      return res.status(400).json({ error: "Invalid round" });
+    }
+
+    return res.status(200).json(filterMatches);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error retrieving fixture" });
+  }
+}
+
 const createFixture = async (req, res) => {
   try {
     const existingFixture = await Fixture.findOne({
@@ -100,6 +171,7 @@ const deleteFixture = async (req, res) => {
 module.exports = {
   getFixture,
   getFixtureByTeamId,
+  getFixtureByStageAndRound,
   createFixture,
   deleteFixture,
 };
--- src/routes/routesFixture.js
+++ src/routes/routesFixture.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getFixture, createFixture, deleteFixture, getFixtureByTeamId } = require("../controllers/fixtureController");
+const { getFixture, createFixture, deleteFixture, getFixtureByTeamId, getFixtureByStageAndRound } = require("../controllers/fixtureController");
 const router = express.Router();
 
 router.get("/:category", getFixture);
 router.get("/:category/:teamId", getFixtureByTeamId);
+router.get("/:category/:stage/:round", getFixtureByStageAndRound);
 router.post("/", createFixture);
 router.delete("/:category", deleteFixture);
 
