746594676661ee7509130cedf8e8826f0d19a157
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-22 16:59:17 -0500

    Refactor Performance routes to enhance endpoint structure and improve clarity

--- src/routes/routesPerformance.js
+++ src/routes/routesPerformance.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const { getPerformance, getPerformanceByTeamId, changePerformanceByTeamId } = require("../controllers/performaceController");
-const router = express.Router();;
+const express = require('express');
+const { getPerformanceByCategory, getPerformanceByTeamId, createPerformance, updatePerformanceByTeamId, deletePerformance } = require('../controllers/performaceController');
+const router = express.Router();
 
-router.get("/:category", getPerformance);
-router.get("/:category/:teamId", getPerformanceByTeamId);
-router.put("/:category/:teamId/:destination", changePerformanceByTeamId);
+router.get('/category/:category', getPerformanceByCategory);
+router.get('/category/:category/teamId/:teamId', getPerformanceByTeamId);
+router.post('/', createPerformance);
+router.put('/:teamId/:phase', updatePerformanceByTeamId);
+router.delete('/:teamId', deletePerformance);
 
 module.exports = router;
