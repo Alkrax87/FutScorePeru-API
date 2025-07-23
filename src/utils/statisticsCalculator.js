0d37a7e2b942403dcb74855d0e42ef50eb34c934
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-23 12:49:59 -0500

    Refactor performance attributes to the new performance structure

--- src/utils/statisticsCalculator.js
+++ src/utils/statisticsCalculator.js
@@ -1,30 +1,30 @@
 function prepareDataForStatistics(data, stage1, stage2) {
   const mergedData = data.map(team => ({
     teamId: team.teamId,
-    pg: team[stage1].pg + team[stage2].pg,
-    pe: team[stage1].pe + team[stage2].pe,
-    pp: team[stage1].pp + team[stage2].pp,
+    w: team[stage1].w + team[stage2].w,
+    d: team[stage1].d + team[stage2].d,
+    l: team[stage1].l + team[stage2].l,
     gf: team[stage1].gf + team[stage2].gf,
-    gc: team[stage1].gc + team[stage2].gc,
-    dg: team[stage1].gf + team[stage2].gf - (team[stage1].gc + team[stage2].gc),
+    ga: team[stage1].ga + team[stage2].ga,
+    gd: team[stage1].gf + team[stage2].gf - (team[stage1].ga + team[stage2].ga),
   }));
 
   return calculateStatistics(structuredClone(mergedData));
 }
 
 function calculateStatistics(data) {
-  const defenseSorted = data.sort((a, b) => a.gc - b.gc);
-  const bestDefenseData = defenseSorted.slice(0, 8).map(({ teamId, gc }) => ({ teamId, gc }));
-  const worstDefenseData = defenseSorted.slice(-8).map(({ teamId, gc }) => ({ teamId, gc })).reverse();
+  const defenseSorted = data.sort((a, b) => a.ga - b.ga);
+  const bestDefenseData = defenseSorted.slice(0, 8).map(({ teamId, ga }) => ({ teamId, ga }));
+  const worstDefenseData = defenseSorted.slice(-8).map(({ teamId, ga }) => ({ teamId, ga })).reverse();
   const goalsForSorted = data.sort((a, b) => b.gf - a.gf);
   const mostGoalsForData = goalsForSorted.slice(0, 8).map(({ teamId, gf }) => ({ teamId, gf }));
   const fewestGoalsForData = goalsForSorted.slice(-8).map(({ teamId, gf }) => ({ teamId, gf })).reverse();
-  const mostWinsData = data.sort((a, b) => b.pg - a.pg).slice(0, 8).map(({ teamId, pg }) => ({ teamId, pg }));
-  const mostDrawsData = data.sort((a, b) => b.pe - a.pe).slice(0, 8).map(({ teamId, pe }) => ({ teamId, pe }));
-  const mostLossesData = data.sort((a, b) => b.pp - a.pp).slice(0, 8).map(({ teamId, pp }) => ({ teamId, pp }));
-  const goalDifferenceSorted = data.sort((a, b) => b.dg - a.dg);
-  const bestGoalDifferenceData = goalDifferenceSorted.slice(0, 8).map(({ teamId, dg }) => ({ teamId, dg }));
-  const worstGoalDifferenceData = goalDifferenceSorted.slice(-8).map(({ teamId, dg }) => ({ teamId, dg })).reverse();
+  const mostWinsData = data.sort((a, b) => b.w - a.w).slice(0, 8).map(({ teamId, w }) => ({ teamId, w }));
+  const mostDrawsData = data.sort((a, b) => b.d - a.d).slice(0, 8).map(({ teamId, d }) => ({ teamId, d }));
+  const mostLossesData = data.sort((a, b) => b.l - a.l).slice(0, 8).map(({ teamId, l }) => ({ teamId, l }));
+  const goalDifferenceSorted = data.sort((a, b) => b.gd - a.gd);
+  const bestGoalDifferenceData = goalDifferenceSorted.slice(0, 8).map(({ teamId, gd }) => ({ teamId, gd }));
+  const worstGoalDifferenceData = goalDifferenceSorted.slice(-8).map(({ teamId, gd }) => ({ teamId, gd })).reverse();
 
   return {
     bestDefense: bestDefenseData,
