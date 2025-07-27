3a95139296df29e7041b315ed3988508c9aa1003
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-27 18:08:42 -0500

    Refactor Results controller to improve data retrieval and management functions

--- src/controllers/resultsController.js
+++ src/controllers/resultsController.js
@@ -1,97 +1,138 @@
-const Team = require("../models/Team");
+const Results = require('../models/Results');
 
-const getResults = async (req, res) => {
+const validPhases = ['apertura', 'clausura', 'regional', 'grupos', 'final'];
+
+const getResultsByCategory = async (req, res) => {
   try {
-    const resultsData = await Team.find({
+    const resultsData = await Results.find({
       category: req.params.category,
-    }).select(
-      "-_id teamId results"
-    );
+    }).select('-_id -category');
 
     if (resultsData.length > 0) {
-      const constructedData = resultsData.map((item) => {
-        const resultsItem = {  teamId: item.teamId };
-        item.results.forEach((element => {
-          resultsItem[element.name] = element.score;
-        }));
-        return resultsItem;
-      });
-
-      return res.status(200).json(constructedData);
+      return res.status(200).json(resultsData);
     } else {
-      return res.status(404).json({ error: "Category not found" });
+      return res.status(404).json({ error: 'Category not found' });
     }
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Error retrieving all results" });
+    console.error(error.message);
+    return res.status(500).json({ error: 'Error retrieving Results' });
   }
 };
 
 const getResultsByTeamId = async (req, res) => {
   try {
-    const resultsData = await Team.findOne({
-      category: req.params.category,
+    const resultsData = await Results.findOne({
       teamId: req.params.teamId,
-    }).select(
-      "-_id teamId results"
-    );
+    }).select('-_id -category');
 
     if (!resultsData) {
-      return res.status(404).json({ error: "Results not found" });
+      return res.status(404).json({ error: 'Results not found' });
     }
 
-    const constructedData = { teamId: resultsData.teamId };
-    resultsData.results.forEach((element) => {
-      constructedData[element.name] = element.score;
-    });
-
-    return res.status(200).json(constructedData);
+    return res.status(200).json(resultsData);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Error retrieving results" });
+    console.error(error.message);
+    return res.status(500).json({ error: 'Error retrieving Results' });
   }
 };
 
-const changeResultsByTeamId = async (req, res) => {
+const createResults = async (req, res) => {
   try {
-    const { destination, index } = req.params;
+    const { teamId, category, phases } = req.body;
 
-    const validDestinations = ["apertura", "clausura", "regional", "grupos", "regular", "final"];
+    const existingResults = await Results.findOne({
+      teamId: teamId,
+    });
 
-    if (!validDestinations.includes(destination)) {
-      return res.status(400).json({ error: "Invalid destination" });
+    if (existingResults) {
+      return res.status(400).json({ error: 'Results already exists' });
+    }
+
+    if (!Array.isArray(phases) || phases.length === 0) {
+      return res.status(400).json({ error: 'Phase must be a non-empty array' });
+    }
+
+    const doc = {
+      teamId,
+      category,
+    };
+
+    for (const phase of phases) {
+      if (!validPhases.includes(phase.name)) {
+        return res.status(400).json({ error: `Invalid phase: ${phase.name}` });
+      }
+
+      if (!isNaN(phase.size) && phase.size > 0) {
+        doc[phase.name] = Array(phase.size).fill(null);
+      }
+    }
+
+    const newResults = new Results(doc);
+    await newResults.save();
+
+    return res.status(201).json({ message: 'Results created successfully' });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Failed to create Results' });
+  }
+}
+
+const updateResultsByTeamId = async (req, res) => {
+  try {
+    const { teamId, phase, index } = req.params;
+
+    if (!validPhases.includes(phase)) {
+      return res.status(400).json({ error: 'Invalid phase' });
     }
 
     const newValue = req.body.score;
 
-    const updatedResults = await Team.findOne({
-      category: req.params.category,
-      teamId: req.params.teamId,
+    const updatedResults = await Results.findOne({
+      teamId: teamId,
     });
 
     if (!updatedResults) {
-      return res.status(404).json({ error: "Results not found" });
+      return res.status(404).json({ error: 'Results not found' });
     }
 
-    const findedResults = updatedResults.results.find((item) => item.name === req.params.destination);
+    const findedResults = updatedResults[phase];
 
     if (!findedResults) {
-      return res.status(404).json({ error: "Destination not found in results" });
+      return res.status(404).json({ error: 'Phase not found in Results' });
     }
 
-    findedResults.score[index - 1] = newValue;
+    findedResults[index] = newValue;
 
     await updatedResults.save();
 
-    return res.status(200).json({ message: "Successfully updated results" });
+    return res.status(200).json({ message: 'Successfully updated results' });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Failed to update results" });
+    console.error(error.message);
+    return res.status(500).json({ error: 'Failed to update results' });
+  }
+};
+
+const deleteResults = async (req, res) => {
+  try {
+    const deletedResults = await Results.findOneAndDelete({
+      teamId: req.params.teamId,
+    });
+
+    if (!deletedResults) {
+      return res.status(404).json({ error: 'Results not found' });
+    }
+
+    return res.status(200).json({ message: 'Results deleted successfully' });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Error retrieving Results' });
   }
 }
 
 module.exports = {
-  getResults,
+  getResultsByCategory,
   getResultsByTeamId,
-  changeResultsByTeamId,
+  createResults,
+  updateResultsByTeamId,
+  deleteResults
 };
