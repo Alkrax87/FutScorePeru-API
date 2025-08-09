4051ed612bc6ac19700ec5d2248824a7cbb389b4
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-08 21:41:03 -0500

    Refactor routesManager: improve endpoint structure and enhance clarity for manager-related operations

--- src/routes/routesManagers.js
+++ src/routes/routesManagers.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const { getManagers, getManagerByTeamId, createManager, updateManager, deleteManager } = require("../controllers/managerController");
+const express = require('express');
+const { getManagers, getManagersByCategory, getManagersByTeamId, createManager, updateManager, deleteManager } = require('../controllers/managerController');
 const router = express.Router();
 
-router.get("/:category", getManagers);
-router.post("/:category", createManager);
-router.get("/:category/:teamId", getManagerByTeamId);
-router.put("/:category/:managerId", updateManager);
-router.delete("/:category/:managerId", deleteManager);
+router.get('/', getManagers);
+router.get('/category/:category', getManagersByCategory);
+router.get('/teamId/:teamId', getManagersByTeamId);
+router.post('/', createManager);
+router.put('/:managerId', updateManager);
+router.delete('/:managerId', deleteManager);
 
 module.exports = router;
