ea847978650b770e5688b3d5b794b217735bec3a
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-08 21:37:26 -0500

    Simple change to keep structure

--- src/models/Manager.js
+++ src/models/Manager.js
@@ -3,8 +3,8 @@ const { dbContent } = require("../config/database");
 
 const ManagerSchema = new Schema(
   {
-    category: { type: Number, required: true },
     managerId: { type: Number, required: true, unique: true },
+    category: { type: Number, required: true },
     teamId: { type: String, required: true },
     name: { type: String, required: true },
     cod: { type: String },
