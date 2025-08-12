a28d9f9f4c2a07eff198dd4e3518c2cdf51220c1
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-11 19:37:10 -0500

    Refactor Map controller to improve data retrieval and management functions

--- src/controllers/mapController.js
+++ src/controllers/mapController.js
@@ -1,47 +1,70 @@
-const Map = require("../models/Map");
+const Map = require('../models/Map');
 
-const getMapElements = async (req, res) => {
+const getMaps = async (req, res) => {
+  try {
+    const mapsData = await Map.find().select('-_id');
+
+    if (!mapsData) {
+      return res.status(404).json({ error: 'Maps not found' });
+    }
+
+    return res.status(200).json(mapsData);
+  } catch (error) {
+    console.error(error.error);
+    return res.status(500).json({ message: 'Error retrieving Maps' });
+  }
+}
+
+const getMapByCategory = async (req, res) => {
   try {
     const mapData = await Map.findOne({
       category: req.params.category,
     }).select(
-      "-_id"
+      '-_id'
     );
 
     if (!mapData) {
-      return res.status(404).json({ error: "Category not found" });
+      return res.status(404).json({ error: 'Map not found' });
     }
 
     return res.status(200).json(mapData.model);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Error al obtener MapElements" });
+    console.error(error.error);
+    return res.status(500).json({ message: 'Error retrieving Map' });
   }
 };
 
 const changeMapElementStatus = async (req, res) => {
-  const updatedMapData = await Map.findOne({
-    category: req.params.category,
-  })
+  try {
+    const updatedMapData = await Map.findOne({
+      category: req.params.category,
+    });
 
-  if (!updatedMapData) {
-    return res.status(404).json({ error: "Category not found" });
+    if (!updatedMapData) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    const findedMapElement = updatedMapData.model.find(
+      (item) => item.mapId === req.params.mapId
+    );
+
+    if (!findedMapElement) {
+      return res.status(404).json({ error: 'MapId not found' });
+    }
+
+    findedMapElement.mapStatus = req.body.mapStatus;
+
+    await updatedMapData.save();
+
+    return res.status(200).json({ message: 'Map updated successfully' });
+  } catch (error) {
+    console.error(error.error);
+    return res.status(500).json({ message: 'Failed to update Map' });
   }
-
-  const findedMapElement = updatedMapData.model.find((item) => item.mapId === req.params.mapId);
-
-  if (!findedMapElement) {
-    return res.status(404).json({ error: "MapId not found" });
-  }
-
-  findedMapElement.mapStatus = !findedMapElement.mapStatus;
-
-  await updatedMapData.save();
-
-  return res.status(200).json({ message: "Successfully updated map element" });
-}
+};
 
 module.exports = {
-  getMapElements,
+  getMaps,
+  getMapByCategory,
   changeMapElementStatus,
 };
