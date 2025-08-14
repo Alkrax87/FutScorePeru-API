0f6442e9a3b57321d59bdee6a1e8260ba5e95624
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-13 21:12:42 -0500

    Refactor Division model schema to set new structure

--- src/models/Division.js
+++ src/models/Division.js
@@ -1,26 +1,50 @@
-const { Schema } = require("mongoose");
-const { dbContent } = require("../config/database");
+const { Schema } = require('mongoose');
+const { dbContent } = require('../config/database');
+
+const BracketSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    status: { type: Boolean, default: false },
+  },
+  { _id: false }
+);
 
 const DivisionSchema = new Schema(
   {
-    category: { type: Number, required: true, unique: true },
-    name: { type: String, required: true },
+    divisionId: { type: Number, required: true },
     sup: { type: String, required: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
     season: { type: Number, required: true },
     teams: { type: Number, required: true },
-    stages: [
-      {
-        _id: false,
-        name: { type: String, required: true },
-        inGame: { type: Number, default: 1 },
-        status: { type: Boolean, default: false },
-      },
-    ],
+    firstPhase: {
+      name: { type: String, required: true },
+      inGame: { type: Number, default: 1 },
+      status: { type: Boolean, default: false },
+    },
+    secondPhase: {
+      name: { type: String, required: true },
+      inGame: { type: Number, default: 1 },
+      status: { type: Boolean, default: false },
+    },
+    thirdPhase: {
+      name: { type: String, required: true },
+      status: { type: Boolean, default: false },
+    },
+    brackets: {
+      bracket32: { type: BracketSchema, default: undefined },
+      bracket16: { type: BracketSchema, default: undefined },
+      bracket8: { type: BracketSchema, default: undefined },
+      bracket4: { type: BracketSchema, default: undefined },
+      bracket2: { type: BracketSchema, default: undefined },
+      bracket1: { type: BracketSchema, default: undefined },
+      bracketExtra: { type: BracketSchema, default: undefined },
+    },
   },
   {
-    collection: "division",
+    collection: 'division',
     versionKey: false,
   }
 );
 
-module.exports = dbContent.model("Division", DivisionSchema);
+module.exports = dbContent.model('Division', DivisionSchema);
