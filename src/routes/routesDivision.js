c1ea430f3a08e18ffeec2d4cb8dd330388042753
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-13 21:16:28 -0500

    Refactor routesDivision to improve endpoints

--- src/routes/routesDivision.js
+++ src/routes/routesDivision.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const { getDivision, createDivision, updateDivision, deleteDivision } = require("../controllers/divisionController");
+const express = require('express');
+const { getDivisions, getDivisionByCategoryId, createDivision, updateDivision, deleteDivision } = require('../controllers/divisionController');
 const router = express.Router();
 
-
-router.get("/:category", getDivision);
-router.post("/", createDivision);
-router.put("/:category", updateDivision);
-router.delete("/:category", deleteDivision);
+router.get('/', getDivisions);
+router.get('/:divisionId', getDivisionByCategoryId);
+router.post('/', createDivision);
+router.put('/:divisionId', updateDivision);
+router.delete('/:divisionId', deleteDivision);
 
 module.exports = router;
