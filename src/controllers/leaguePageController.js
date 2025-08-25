fd5b26077fb1f2e24f9aa95d4a766c49f9195611
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-24 19:07:54 -0500

    Refactor getLeagueInformation to add teamId filtering data

--- src/controllers/leaguePageController.js
+++ src/controllers/leaguePageController.js
@@ -1,37 +1,33 @@
-const League = require("../models/League");
+const League = require('../models/League');
+const teamCPFinder = require('../utils/teamCPFinder');
 
 const getLeagueInformation = async (req, res) => {
   try {
     const leagueData = await League.findOne({
       leagueId: req.params.leagueId,
     }).select(
-      "-_id location image teams information"
+      '-_id image alt location teams information'
     );
 
     if (!leagueData) {
-      return res.status(404).json({ error: "League not found" });
+      return res.status(404).json({ error: 'League not found' });
     }
 
+    const teams = await teamCPFinder(leagueData.teams);
+
     return res.status(200).json({
-      title: leagueData.location,
-      flag: leagueData.image,
-      foundation: leagueData.information.foundation,
-      teams: [
-        ...leagueData.teams.map(team => ({
-          name: team.name,
-          image: team.image || null,
-        }))
-      ],
-      topWinner: leagueData.information.topWinner,
-      leagues: leagueData.information.leagues,
-      allTimeWinners: leagueData.information.allTimeWinners,
+      image: leagueData.image,
+      alt: leagueData.alt,
+      location: leagueData.location,
+      teams: teams.map(({ name, abbreviation, image }) => ({ name, abbreviation, image })),
+      information: leagueData.information,
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Error retrieving league information" });
+    return res.status(500).json({ error: 'Error retrieving league information' });
   }
-}
+};
 
 module.exports = {
-  getLeagueInformation
-}
+  getLeagueInformation,
+};
