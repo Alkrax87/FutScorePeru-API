1d0b1ecb909ead8e8f191c8bca2443104b40b6ca
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-25 18:09:10 -0500

    Refactor League controller yo use TeamCPFinder and get TeamCP data, remove create function

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -1,63 +1,72 @@
-const League = require("../models/League");
+const League = require('../models/League');
+const teamCPFinder = require('../utils/teamCPFinder');
 
 const getLeagues = async (req, res) => {
   try {
     const leaguesData = await League.find().select(
-      "-_id category leagueId region image imageThumbnail alt location color teams"
+      '-_id category leagueId region image imageThumbnail alt location color teams'
     );
 
     if (leaguesData.length > 0) {
-      return res.status(200).json(leaguesData);
+      const leaguesWithTeams = await Promise.all(
+        leaguesData.map(async (league) => {
+          const teams = await teamCPFinder(league.teams);
+
+          return {
+            leagueId: league.leagueId,
+            category: league.category,
+            image: league.image,
+            imageThumbnail: league.imageThumbnail,
+            alt: league.alt,
+            location: league.location,
+            color: league.color,
+            teams: teams.filter(Boolean),
+          };
+        })
+      );
+
+      return res.status(200).json(leaguesWithTeams);
     } else {
-      return res.status(404).json({ error: "No leagues found" });
+      return res.status(404).json({ error: 'No leagues found' });
     }
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Error retrieving leagues" });
+    return res.status(500).json({ error: 'Error retrieving leagues' });
   }
 };
 
 const getLeagueById = async (req, res) => {
   try {
-    const leagueData = await League.findOne({
+    leagueData = await League.findOne({
       leagueId: req.params.leagueId,
     }).select(
-      "-_id category leagueId region image imageThumbnail alt location color teams"
+      '-_id category leagueId image imageThumbnail alt location color teams'
     );
 
     if (!leagueData) {
-      return res.status(404).json({ error: "League not found" });
+      return res.status(404).json({ error: 'League not found' });
     }
 
-    return res.status(200).json(leagueData);
+    const teams = await teamCPFinder(leagueData.teams);
+
+    const leagueWithTeams = {
+      leagueId: leagueData.leagueId,
+      category: leagueData.category,
+      image: leagueData.image,
+      imageThumbnail: leagueData.imageThumbnail,
+      alt: leagueData.alt,
+      location: leagueData.location,
+      teams: teams,
+    };
+
+    return res.status(200).json(leagueWithTeams);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Error retrieving league" });
+    return res.status(500).json({ error: 'Error retrieving league' });
   }
 };
-
-const createLeague = async (req, res) => {
-  try {
-    const exitingLeague = await League.findOne({
-      leagueId: req.body.leagueId,
-    })
-
-    if (exitingLeague) {
-      return res.status(400).json({ error: "League already exists" });
-    }
-
-    const newLeague = new League(req.body);
-    await newLeague.save();
-
-    return res.status(201).json({ message: "League created successfully" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Error creating league" });
-  }
-}
 
 module.exports = {
   getLeagues,
   getLeagueById,
-  createLeague,
 };
