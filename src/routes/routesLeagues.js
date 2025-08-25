df7e3b26f89522c8ab63b5456dd7a1b5c7bc4f07
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-25 18:11:05 -0500

    Remove POST endpoint

--- src/routes/routesLeagues.js
+++ src/routes/routesLeagues.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { getLeagues, getLeagueById, createLeague } = require('../controllers/leagueController');
+const { getLeagues, getLeagueById } = require('../controllers/leagueController');
 const router = express.Router();
 
 router.get('/', getLeagues);
 router.get('/:leagueId', getLeagueById);
-router.post('/', createLeague);
 
 module.exports = router;
