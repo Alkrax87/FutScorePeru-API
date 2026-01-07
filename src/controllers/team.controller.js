40eac78efab177f31ca6dee2269909d74e039872
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-06 19:51:46 -0500

    Refactor team controller and model: update field names, enhance sorting, improve error handling and change naming convention

--- src/controllers/team.controller.js
+++ src/controllers/team.controller.js
@@ -1,79 +1,63 @@
 const Team = require('../models/Team');
 
-const getTeams = async (req, res) => {
+module.exports.getTeams = async (req, res) => {
   try {
-    const teamsData = await Team.find().select('-_id');
+    const teamsData = await Team.find().select('-_id').sort({ category: 1, name: 1 });
 
-    if (teamsData.length > 0) {
-      return res.status(200).json(teamsData);
-    } else {
-      return res.status(404).json({ error: 'Teams not found' });
-    }
+    return res.status(200).json(teamsData);
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Error retrieving Teams' });
   }
 };
 
-const getTeamsByCategory = async (req, res) => {
+module.exports.getTeamsByCategory = async (req, res) => {
   try {
-    const teamsData = await Team.find({
-      category: req.params.category
-    }).select(
-      '-_id'
-    );
+    const teamsData = await Team.find({ category: req.params.category }).select('-_id').sort({ name: 1 });
 
-    if (teamsData.length > 0) {
-      return res.status(200).json(teamsData);
-    } else {
-      return res.status(404).json({ error: 'Teams not found' });
-    }
+    return res.status(200).json(teamsData);
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Error retrieving Teams' });
   }
 };
 
-const getTeamById = async (req, res) => {
+module.exports.getTeamByTeamId = async (req, res) => {
   try {
-    const teamData = await Team.findOne({
-      teamId: req.params.teamId,
-    }).select(
-      '-_id'
-    );
+    const teamData = await Team.findOne({ teamId: req.params.teamId }).select('-_id');
 
-    if (!teamData) {
-      return res.status(404).json({ error: 'Team not found' });
+    if (teamData) {
+      return res.status(200).json(teamData);
     }
 
-    return res.status(200).json(teamData);
+    return res.status(404).json({ error: 'Team not found' });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Error retrieving Team' });
   }
 };
 
-const createTeam = async (req, res) => {
+module.exports.createTeam = async (req, res) => {
   try {
-    const existingTeam = await Team.findOne({
-      teamId: req.body.teamId,
-    });
-
-    if (existingTeam) {
-      return res.status(400).json({ error: 'Team already exists' });
-    }
-
     const newTeam = new Team(req.body);
     await newTeam.save();
 
     return res.status(201).json({ message: 'Team added successfully' });
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ error: 'Team already exists' });
+    }
+
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ error: error.message });
+    }
+
     console.error(error.message);
     return res.status(500).json({ error: 'Failed to create Team' });
   }
 };
 
-const updateTeam = async (req, res) => {
+module.exports.updateTeam = async (req, res) => {
   try {
     const updatedTeam = await Team.findOneAndUpdate(
       { teamId: req.params.teamId },
@@ -81,12 +65,13 @@ const updateTeam = async (req, res) => {
         $set: {
           teamId: req.body.teamId,
           category: req.body.category,
-          groupFirstPhase: req.body.groupFirstPhase,
-          groupSecondPhase: req.body.groupSecondPhase,
+          groupPhase1: req.body.groupFirstPhase,
+          groupPhase2: req.body.groupSecondPhase,
           name: req.body.name,
           abbreviation: req.body.abbreviation,
           image: req.body.image,
           imageThumbnail: req.body.imageThumbnail,
+          background: req.body.background,
           alt: req.body.alt,
           location: req.body.location,
           stadium: req.body.stadium,
@@ -102,16 +87,22 @@ const updateTeam = async (req, res) => {
 
     return res.status(200).json({ message: 'Team updated successfully' });
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ error: 'Team already exists' });
+    }
+
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ error: error.message });
+    }
+
     console.error(error.message);
     return res.status(500).json({ error: 'Failed to update Team' });
   }
 };
 
-const deleteTeam = async (req, res) => {
+module.exports.deleteTeam = async (req, res) => {
   try {
-    const deletedTeam = await Team.findOneAndDelete({
-      teamId: req.params.teamId,
-    });
+    const deletedTeam = await Team.findOneAndDelete({ teamId: req.params.teamId });
 
     if (!deletedTeam) {
       return res.status(404).json({ error: 'Team not found' });
@@ -122,13 +113,4 @@ const deleteTeam = async (req, res) => {
     console.error(error.message);
     return res.status(500).json({ error: 'Failed to delete Team' });
   }
-};
-
-module.exports = {
-  getTeams,
-  getTeamsByCategory,
-  getTeamById,
-  createTeam,
-  updateTeam,
-  deleteTeam,
 };
--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getTeams, getTeamsByCategory, getTeamById, createTeam, updateTeam, deleteTeam } = require('../controllers/teamController');
+const { getTeams, getTeamsByCategory, getTeamByTeamId, createTeam, updateTeam, deleteTeam } = require('../controllers/team.controller');
 const router = express.Router();
 
 router.get('/', getTeams);
 router.get('/category/:category', getTeamsByCategory);
-router.get('/teamId/:teamId', getTeamById);
+router.get('/teamId/:teamId', getTeamByTeamId);
 router.post('/', createTeam);
 router.put('/:teamId', updateTeam);
 router.delete('/:teamId', deleteTeam);
