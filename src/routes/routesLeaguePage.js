f5aa1ccf8d72d0064361a21121ac3a7a5486e3d8
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-02-26 20:40:37 -0500

    Remove obsolete leaguePage controller and routes

--- src/routes/routesLeaguePage.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getLeagueInformation } = require('../controllers/leaguePageController');
-const router = express.Router();
-
-router.get('/:leagueId', getLeagueInformation);
-
-module.exports = router;
